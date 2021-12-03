24428dfb360c8640a7c059c9a3d3a55a712a823d
Alina <alinasag2@gmail.com>
2021-12-03 14:17:52 -0600

    #3 try catch added

---
+++ P3_Guess_The_Number/src/com/company/Main.java
@@ -0,0 +1,49 @@
+package com.company;
+
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello!What is your name?");
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
+        System.out.println("\n" + name);
+
+        System.out.println("Well," + name + "," + "I am thinking of a number between 1 and 20. Take a guess");
+
+        Random ran = new Random();
+        int number = ran.nextInt(20);
+        int guess = 0;
+        int count = 0;
+        String yes = "";
+        String res = "";
+
+        do {
+            try {
+                guess = scan.nextInt();
+                if (guess == number) {
+                    System.out.println("Good job," + name + "!" + "You guessed my number in " + count + " guesses!"
+                            + "Would you like to play again? (y or n)");
+                    yes = scan.nextLine();
+                    res += yes;
+                } else if (guess < number && guess<21) {
+                    System.out.println("Your guess is too low. Take a guess.");
+                    count++;
+                } else if (guess > number && guess<21) {
+                    System.out.println("Your guess is too high. Take a guess.");
+                    count++;
+                }
+                System.out.println("Invalid number,enter the number between 1-20");
+            }
+            catch(Exception e){
+                System.out.println("Invalid number,enter the number between 1-20");
+            }
+            yes = scan.nextLine();
+            res += yes;
+        } while (guess != number || res.equals("y")) ;
+
+    }
+}
